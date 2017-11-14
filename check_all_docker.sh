dcc689a1aa5d8cfea535eb67a0258645947ee53a
Frederic LAURENT <frederic.laurent@gmail.com>
2017-11-14 16:12:03 +0100

    utilisation conteneur ameli_checker

--- check_all_docker.sh
+++ check_all_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Verification des versions disponibles des referentiels LPP, UCD, CCAM"
 
-docker run -t -v "$PWD":/opt -w /opt py_ameli sh ./check_ref.sh lpp
-docker run -t -v "$PWD":/opt -w /opt py_ameli sh ./check_ref.sh ucd
-docker run -t -v "$PWD":/opt -w /opt py_ameli sh ./check_ref.sh ccam
+docker exec ameli_checker sh ./check_ref.sh lpp
+docker exec ameli_checker sh ./check_ref.sh ucd
+docker exec ameli_checker sh ./check_ref.sh ccam
