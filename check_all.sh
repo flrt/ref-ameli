a9d14cb62b95a2e0c430effdf2eccbf30e0075f5
Frederic Laurent <frederic.laurent@gmail.com>
2018-01-02 18:47:03 +0100

    détection de la NABM

--- check_all.sh
+++ check_all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-echo "Verification des versions disponibles des referentiels LPP, UCD, CCAM"
+echo "Verification des versions disponibles des referentiels LPP, UCD, CCAM, NABM"
 
 sh ./check_ref.sh lpp
 sh ./check_ref.sh ucd
 sh ./check_ref.sh ccam
+sh ./check_ref.sh nabm
--- check_all_docker.sh
+++ check_all_docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-echo "Verification des versions disponibles des referentiels LPP, UCD, CCAM"
+echo "Verification des versions disponibles des referentiels LPP, UCD, CCAM, NABM"
 
 docker exec ameli_checker sh ./check_ref.sh lpp
 docker exec ameli_checker sh ./check_ref.sh ucd
 docker exec ameli_checker sh ./check_ref.sh ccam
+docker exec ameli_checker sh ./check_ref.sh nabm
