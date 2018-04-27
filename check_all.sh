b2464a52509fdeca69340f25929f250c3c7cb42e
Frederic Laurent <frederic.laurent@gmail.com>
2018-04-27 17:22:31 +0200

    ajout path d'execution dans le bash

--- check_all.sh
+++ check_all.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 echo "Verification des versions disponibles des referentiels LPP, UCD, CCAM, NABM"
 
+RUNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $RUNDIR
+
 sh ./check_ref.sh lpp
 sh ./check_ref.sh ucd
 sh ./check_ref.sh ccam
